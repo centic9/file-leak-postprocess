caba0f46aa34620815278f9e046a15f4026c9867
Dominik Stadler <dominik.stadler@gmx.at>
2022-03-12 14:20:25 +0100

    Check for arguments in run-script

--- run.sh
+++ run.sh
@@ -2,6 +2,15 @@
 
 set -eu
 
+if [ $# -lt 1 ]; then
+  echo Specify one or more text-files with stacktraces produced by file-leak-detector
+  echo
+  echo "file-leak-postprocess <text-file> [<text-file> ...]"
+  echo
+
+  exit 1
+fi
+
 # run file-leak-postprocess with the given files
 ./gradlew check installDist
 
