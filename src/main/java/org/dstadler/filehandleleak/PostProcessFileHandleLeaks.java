993e62184fd9b9299054627d02811a0f072872a8
Dominik Stadler <dominik.stadler@gmx.at>
2022-03-12 13:52:28 +0100

    Add output of combined stacktraces to stdout

--- src/main/java/org/dstadler/filehandleleak/PostProcessFileHandleLeaks.java
+++ src/main/java/org/dstadler/filehandleleak/PostProcessFileHandleLeaks.java
@@ -7,6 +7,9 @@ import org.dstadler.commons.arrays.ArrayUtils;
 import org.dstadler.commons.collections.MappedCounter;
 import org.dstadler.commons.collections.MappedCounterImpl;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
 public class PostProcessFileHandleLeaks {
     public static void main(String[] args) throws Exception {
         if(args.length == 0) {
@@ -35,13 +38,26 @@ public class PostProcessFileHandleLeaks {
         }
 
 		// de-duplicate stacktraces
+		Multimap<String, FileHandleLeak> leaksByStacktrace = HashMultimap.create();
 		MappedCounter<String> uniqueStacks = new MappedCounterImpl<>();
 		for (FileHandleLeak leak : leaks) {
 			uniqueStacks.inc(leak.getStacktrace());
+			leaksByStacktrace.put(leak.getStacktrace(), leak);
 		}
 
+		// print an overview to stderr
 		System.err.println("Found " + leaks.size() + " file-handle-leaks in " +
 				ArrayUtils.toString(args, ", ", "", ""));
 		System.err.println("Had " + uniqueStacks.sortedMap().keySet().size() + " unique stacktraces: " + uniqueStacks.sortedMap().values());
+
+		// print combined stacktraces to stdout
+		for (String stackTrace : leaksByStacktrace.keySet()) {
+			// print all headers
+			for (FileHandleLeak fileHandleLeak : leaksByStacktrace.get(stackTrace)) {
+				System.out.println(fileHandleLeak.getHeader());
+			}
+
+			System.out.println(stackTrace);
+		}
     }
 }
