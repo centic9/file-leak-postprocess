558c8711a9c750e67d6666f9cdd0dc9f18924f72
Dominik Stadler <dominik.stadler@gmx.at>
2022-03-12 13:52:36 +0100

    Add more output to script

--- run.sh
+++ run.sh
@@ -3,4 +3,13 @@
 set -eu
 
 # run file-leak-postprocess with the given files
-./gradlew check installDist && build/install/file-leak-postprocess/bin/file-leak-postprocess "$@" > /tmp/file-handle-leaks.txt
+./gradlew check installDist
+
+echo
+build/install/file-leak-postprocess/bin/file-leak-postprocess "$@" > /tmp/file-handle-leaks.txt
+
+echo
+echo Written combined stacktraces to /tmp/file-handle-leaks.txt
+ls -al /tmp/file-handle-leaks.txt
+
+exit 0
