60afb38bc89499eee334d2d57b62830fac6ad610
Dominik Stadler <dominik.stadler@gmx.at>
2022-03-12 09:40:18 +0100

    Fix running application

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -eu
 
 # run file-leak-postprocess with the given files
-./gradlew  installDist && build/install/file-leak-postprocess/bin/file-leak-postprocess "$@" > /tmp/file-handle-leaks.txt
+./gradlew check installDist && build/install/file-leak-postprocess/bin/file-leak-postprocess "$@" > /tmp/file-handle-leaks.txt
